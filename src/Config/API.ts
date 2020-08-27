14bae8b3d6f40584469824b1a7a892272dc92a1e
lvxianlei <704894244@qq.com>
2020-08-27 23:56:55 +0800

    Edit优化

--- src/Config/API.ts
+++ src/Config/API.ts
@@ -4,3 +4,5 @@ export const LOGIN_URL: string = BASE_URL + '/login'
 
 export const LIST_URL: string = BASE_URL + '/list'
 
+export const EDIT_URL: string = BASE_URL + '/edit'
+
--- src/component/Edit/component/Edit.tsx
+++ src/component/Edit/component/Edit.tsx
@@ -1,37 +1,78 @@
-import React from 'react'
-import { Form, Input, InputNumber, Button } from 'antd'
+import React, { useEffect, useReducer } from 'react'
+import { Form, Input, InputNumber, Button, Spin, Row, Col } from 'antd'
 import { createFromIconfontCN } from '@ant-design/icons'
+import { EDIT_URL } from '../../../Config/API'
+import { request } from '../../../Util'
 import '../index.scss'
 const IconFont: any = createFromIconfontCN({
     scriptUrl: '//at.alicdn.com/t/font_2014371_vucntjl36is.js'
 })
 
+const initState = {
+    data: [],
+    loading: true
+}
+
+const reducer = (state: any, action: any) => {
+    switch (action.type) {
+        case "FETCH_EDIT_START":
+            return { ...state, loading: false }
+        case "FETCH_EDIT_SUCCESS":
+            return { ...state, data: action.paload }
+        case "FETCH_EDIT_ERROR":
+            return { ...state, error: action.paload }
+    }
+}
+
+const layout = {
+    labelCol: {
+        xs: { span: 12 },
+        sm: { span: 4 },
+    },
+    wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 24 },
+    },
+}
+
 export default (props: any) => {
     const { type } = props.match.params
+    const [editData, dispatch] = useReducer(reducer, initState)
+    useEffect(() => {
+        (async () => {
+            dispatch({ type: 'FETCH_EDIT_START' })
+            try {
+                const editData: any = await request.post(EDIT_URL, { type })
+                dispatch({ type: 'FETCH_EDIT_SUCCESS', paload: editData.data })
+            } catch (error) {
+                dispatch({ type: 'FETCH_EDIT_ERROR', paload: error })
+            }
+        })()
+    }, [dispatch, type])
+
     const onFinish: Function = (value: any) => {
         console.log(value, '------submit------')
     }
 
     return (
-        <Form name="nest-messages" onFinish={event => onFinish}>
-            <Form.Item name={['user', 'name']} label="Name" rules={[{ required: true }]}>
-                <Input />
-            </Form.Item>
-            <Form.Item name={['user', 'email']} label="Email" rules={[{ type: 'email' }]}>
-                <Input />
-            </Form.Item>
-            <Form.Item name={['user', 'age']} label="Age" rules={[{ type: 'number', min: 0, max: 99 }]}>
-                <InputNumber />
-            </Form.Item>
-            <Form.Item name={['user', 'website']} label="Website">
-                <Input />
-            </Form.Item>
-            <Form.Item name={['user', 'introduction']} label="Introduction">
-                <Input.TextArea />
-            </Form.Item>
-            <Form.Item >
-                <Button type="primary" htmlType="submit">确定</Button>
-            </Form.Item>
-        </Form>
+        <Spin spinning={editData.loading}>
+            <Form name="nest-messages" {...layout} labelAlign="right" onFinish={event => onFinish(event)}>
+                <Row>
+                    {editData.data.map((item: any) =>
+                        <Col span={9} offset={2}>
+                            <Form.Item name={item.name} label={item.label} rules={[{ required: true }]}>
+                                <Input />
+                            </Form.Item>
+                        </Col>
+                    )}
+                </Row>
+                <Row>
+                    <Col span={12} style={{ textAlign: 'right' }}>
+                        <Button type="primary" htmlType="submit" className="login-form-button">确认</Button>
+                        <Button style={{ marginLeft: "10px" }} type="primary" htmlType="submit" className="login-form-button">取消</Button>
+                    </Col>
+                </Row>
+            </Form>
+        </Spin>
     )
 }
